07a3aa3ad7d1e0ce6c999a2ad03468a5e2ae902a
Albert Rovira <diphone2020@gmail.com>
2025-02-14 09:28:34 +0100

    s1 homework done

--- src/HelloWorldClient.java
+++ src/HelloWorldClient.java
@@ -4,25 +4,37 @@ import java.net.Socket;
 
 public class HelloWorldClient {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        // check java version
         String javaVersion = System.getProperty("java.version");
         System.out.println("Versión de Java: " + javaVersion);
 
         String host = "localhost";
         int port = 12345;
 
+//        host address from args
+        if (args.length > 0) {
+            host = args[0];
+        }
+
         try {
             Socket s = new Socket(host, port);
             OutputStream os = s.getOutputStream();
             DataOutputStream dos = new DataOutputStream(os);
 
-            dos.writeUTF("hellow");
+//            sending different data types
+            dos.writeInt (123456);
+            dos.writeDouble (3.141592653589793);
+            dos.writeBoolean (true);
+            dos.writeUTF ("Hellow world!");
+
+            System.out.println("Tot enviat.");
+            dos.flush();
+            dos.close();
             s.close();
 
         }catch (Exception e){
             System.out.println("Ha ocorregut un error i paro");
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
--- src/HelloWorldServer.java
+++ src/HelloWorldServer.java
@@ -1,29 +1,60 @@
 import java.io.DataInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class HelloWorldServer {
-    // crear el main
-    public static void main(String[] args){
-        System.out.println("Soc el server");
+    private static int port = 12345;
 
+    // crear el main
+    public static void main(String[] args) {
         try {
             // obrir el port
-            ServerSocket ss = new ServerSocket(12345);
+            ServerSocket ss = new ServerSocket(port);
             Socket entrada = ss.accept();
             // crear el DIS
             InputStream is = entrada.getInputStream();
             DataInputStream dis = new DataInputStream(is);
-            // llegir l'String
+
+//            llegir un enter (2 variants)
+//            int i = dis.readInt();
+//            System.out.println ("El nombre enter és " +  i);
+
+            byte b3 = dis.readByte();
+            byte b2 = dis.readByte();
+            byte b1 = dis.readByte();
+            byte b0 = dis.readByte();
+            System.out.println("Bytes de l'enter: " + b3 + " " + b2 + " " + b1 + " " + b0);
+
+//            llegir un double
+            double d = dis.readDouble();
+            System.out.println("El nombre real és " + d);
+
+//            llegir un boolean (2 variants)
+            // boolean b = dis.readBoolean();
+            // System.out.println ("El booleà és " +  b);
+            byte b = dis.readByte();
+            System.out.println("El byte del booleà és " + b);
+
+            // llegir l'String (2 variants)
             // pintar-lo per pantalla
-            System.out.println(dis.readUTF());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+//            System.out.println(dis.readUTF());
+
+            short mida = dis.readShort();
+            System.out.println("La mida del text és " + mida);
+            for (int i = 0; i < mida; i++) {
+                byte c = dis.readByte();
+                System.out.print((char) c);
+            }
+            System.out.println();
+
+//            tancament de canals
+            dis.close();
+            entrada.close();
+            ss.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
     }
-
 }
